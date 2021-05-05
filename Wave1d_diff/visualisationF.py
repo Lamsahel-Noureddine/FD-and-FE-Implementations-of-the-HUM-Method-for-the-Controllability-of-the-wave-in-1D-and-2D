34b8e8042802650ba95b030ee31830f489ab936b
Lamsahel-Noureddine <74387695+Lamsahel-Noureddine@users.noreply.github.com>
2021-05-05 01:38:41 -1200

    Update visualisationF.py

--- Wave1d_diff/visualisationF.py
+++ Wave1d_diff/visualisationF.py
@@ -7,13 +7,24 @@ Created on Mon May  3 15:03:19 2021
 import numpy as np
 from Functions_use import *
 
-def visua_app_ana(A_final,x,t):
+def visuawav_app_ana(A_final,x,t):
     N=np.size(x)-2
     M=np.size(t)-2
-    filevisua=open("tabvisualisation_1d_diff.dat",'w')
+    filevisua=open("tabvisualisation_1dwave_diff.dat",'w')
     for n in range(0,M+2):
         for j in range(0,N):
-            filevisua.write("%f \t %f \t %f \t %f \n"%(x[j+1],t[n],phi_exact(x[j+1],t[n]),A_final[j,n]))
+            filevisua.write("%f  \t %f \t %f \t %f \n"%(x[j+1],t[n],phi_exact(x[j+1],t[n]),A_final[j+1,n]))
             
     filevisua.close()
-    filevisua=open("tabvisualisation_1d_diff.dat",'r')
+    filevisua=open("tabvisualisation_1dwave_diff.dat",'r')
+    
+def visuaLap_app_ana(phitest,x):
+    N=np.size(x)-2
+    filevisua=open("tabvisualisation_1dlap_diff.dat",'w')
+    for j in range(1,N+1):
+        filevisua.write("%f  \t %f \t %f  \n"%(x[j],phi_exact_lapl(x[j]),phitest[j]))
+            
+    filevisua.close()
+
+    filevisua=open("tabvisualisation_1dlap_diff.dat",'r')
+    
