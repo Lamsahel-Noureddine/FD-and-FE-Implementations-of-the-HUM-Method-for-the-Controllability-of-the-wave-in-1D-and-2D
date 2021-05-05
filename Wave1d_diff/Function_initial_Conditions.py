62b6d8c803dd043e025791024c77aa1178c7e60f
Lamsahel-Noureddine <74387695+Lamsahel-Noureddine@users.noreply.github.com>
2021-05-05 01:33:42 -1200

    Update Function_initial_Conditions.py

--- Wave1d_diff/Function_initial_Conditions.py
+++ Wave1d_diff/Function_initial_Conditions.py
@@ -6,14 +6,16 @@ Created on Mon May  3 13:21:28 2021
 """
 import numpy as np
 
+############## test of solve1d_wave ##############
 
-#The initial conditions
+#The initial conditions 
 
 def v(x):              # phi^0
     return np.sin(np.pi*x)
 
 def w(x):          # phi^1
     return np.sqrt(2)*np.sin(np.pi*x)
+
 
 
 
