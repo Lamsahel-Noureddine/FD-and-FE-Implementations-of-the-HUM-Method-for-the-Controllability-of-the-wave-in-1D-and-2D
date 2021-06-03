cc592b2f1bd60579ad6a7b1201214ad2c1045bdc
Lamsahel-Noureddine <74387695+Lamsahel-Noureddine@users.noreply.github.com>
2021-06-03 07:35:57 -1200

    Update Functions_use.py

--- Wave1d_diff/Functions_use.py
+++ Wave1d_diff/Functions_use.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Mon May  3 14:38:39 2021
@@ -6,14 +7,21 @@ Created on Mon May  3 14:38:39 2021
 """
 import numpy as np
 
+############## test of solve1d_wave ##############
+ 
 #The boundary condition at 1
 def u(x):
-    return 0*x
+    return 0.*x
 
-#exact solution 
+#exact solution of the wave equation 
 def phi_exact(x,t):
-    term1=   np.sin(np.pi*(x+t))+np.sin(np.pi*(x-t))
-    term2= (-1)*(np.sqrt(2)/np.pi)*np.cos(np.pi*(x+t)) +(np.sqrt(2)/np.pi)*np.cos(np.pi*(x-t))  
-    return (1/2)*(term1+term2)
+    
+    return (1./2)*( np.sin(np.pi*(x+t)) + np.sin(np.pi*(x-t)) )
 
+
+############## test of solve1d_Laplacian ##############
+
+#exact solution of the laplacian  equation
+def phi_exact_lapl(x):
+    return (-1./2)*(x**2 -x )
 
